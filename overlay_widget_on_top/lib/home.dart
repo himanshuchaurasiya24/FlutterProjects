e2687faa7556479513ef233a9e1006ec39eb5164
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-05-23 11:25:58 +0530

    overlay_widget_not_updating_textbox_text_on_tap

---
+++ overlay_widget_on_top/lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:overlay_widget_on_top/text_field_widget.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(32),
+        children: [
+          Container(
+            height: 120,
+            color: Colors.blue,
+          ),
+          Container(
+            height: 120,
+            color: Colors.orange,
+          ),
+          Container(
+            height: 120,
+            color: Colors.red,
+          ),
+          Container(
+            height: 120,
+            color: Colors.green,
+          ),
+          Container(
+            height: 120,
+            color: Colors.pink,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFieldWidget(),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 120,
+            color: Colors.indigo,
+          ),
+          Container(
+            height: 120,
+            color: Colors.teal,
+          ),
+          Container(
+            height: 120,
+            color: Colors.purple,
+          ),
+          Container(
+            height: 120,
+            color: Colors.cyan,
+          ),
+          Container(
+            height: 120,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ overlay_widget_on_top/lib/text_field_widget.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatefulWidget {
+  const TextFieldWidget({super.key});
+
+  @override
+  State<TextFieldWidget> createState() => _TextFieldWidgetState();
+}
+
+class _TextFieldWidgetState extends State<TextFieldWidget> {
+  final controller = TextEditingController();
+  OverlayEntry? entry;
+  final focusNode = FocusNode();
+  final layerLink = LayerLink();
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        _showOverLay();
+      } else {
+        hideOverLay();
+      }
+    });
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _showOverLay(),
+    );
+  }
+
+  void _showOverLay() {
+    final overLay = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+    entry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+            width: size.width,
+            child: CompositedTransformFollower(
+                link: layerLink,
+                offset: Offset(0, size.height + 8),
+                showWhenUnlinked: false,
+                child: buildOverLay()));
+      },
+    );
+    overLay.insert(entry!);
+  }
+
+  void hideOverLay() {
+    entry?.remove();
+    entry = null;
+  }
+
+  Widget buildOverLay() {
+    return Material(
+      elevation: 8,
+      borderRadius: BorderRadius.circular(20),
+      child: Column(
+        children: [
+          ListTile(
+            onTap: () {
+              setState(() {
+                controller.text = 'Sarah Abs';
+              });
+              hideOverLay();
+              focusNode.unfocus();
+            },
+            leading: Image.network(
+              'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=is&k=20&c=M-zkBx55YCn_UZg9cyuVMm1nS_PhSKGqoVybgO1Dp0Y=',
+              fit: BoxFit.cover,
+            ),
+            title: const Text('Sarah Abs'),
+          ),
+          ListTile(
+            onTap: () {
+              setState(() {
+                controller.text = 'Julia Abs';
+              });
+              hideOverLay();
+              focusNode.unfocus();
+            },
+            leading: Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZTJ9PiXYRPBIWO2maHbR9UZHFId3Jj0aDTYR6uXROOA&s',
+              fit: BoxFit.cover,
+            ),
+            title: const Text('Julia Abs'),
+          ),
+          ListTile(
+            onTap: () {
+              setState(() {
+                controller.text = 'Emma Abs';
+              });
+              hideOverLay();
+              focusNode.unfocus();
+            },
+            leading: Image.network(
+              'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80',
+              fit: BoxFit.cover,
+            ),
+            title: const Text('Emma Abs'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: layerLink,
+      child: TextField(
+        focusNode: focusNode,
+        controller: controller,
+        decoration: InputDecoration(
+          label: const Text('Username'),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
