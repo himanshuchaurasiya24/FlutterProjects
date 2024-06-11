177bba8a98acdd7f14db3742f49367630e7dc570
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-06-11 22:55:09 +0530

    chart_flutter_fly

--- grid_view_ws_cube/lib/flutter_range_slider_my.dart
+++ grid_view_ws_cube/lib/flutter_range_slider_my.dart
@@ -8,28 +8,36 @@ class FlutterRangeSliderMy extends StatefulWidget {
 }
 
 class _FlutterRangeSliderMyState extends State<FlutterRangeSliderMy> {
-  RangeValues values = const RangeValues(0, 100);
+  RangeValues values = const RangeValues(100000000, 9999999999);
   @override
   Widget build(BuildContext context) {
     RangeLabels labels =
         RangeLabels(values.start.toString(), values.end.toString());
     return Scaffold(
       body: Center(
-        child: RangeSlider(
-          values: values,
-          labels: labels,
-          min: 0,
-          max: 100,
-          divisions: 20,
-          activeColor: Colors.green[300],
-          inactiveColor: Colors.red[400],
-          overlayColor: WidgetStateColor.transparent,
-          onChanged: (newValue) {
-            setState(() {
-              values = newValue;
-            });
-            debugPrint(newValue.start.toString() + newValue.end.toString());
-          },
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            const Text('Slide your phone number:'),
+            RangeSlider(
+              values: values,
+              labels: labels,
+              min: 100000000,
+              max: 9999999999,
+              divisions: 2,
+              activeColor: Colors.green[300],
+              inactiveColor: Colors.red[400],
+              overlayColor: WidgetStateColor.transparent,
+              onChanged: (newValue) {
+                setState(() {
+                  values = newValue;
+                });
+                debugPrint(newValue.start.toString() + newValue.end.toString());
+              },
+            ),
+          ],
         ),
       ),
     );
